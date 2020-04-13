d6d2392721609eec28170ccd318d8c90f5665062
Martyna Piórkowska <martynapiorkowska@macbook-pro-martyna.home>
2020-04-13 20:44:25 +0200

    Added FizzBuzz

---
+++ src/FizzBuzz/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("FizzBuzz");
+            var textNumber = Console.ReadLine();
+            var number = Convert.ToInt32(textNumber);
+
+            if (number % 3 == 0 && number % 5 == 0)
+            {
+                Console.WriteLine("FizzBuzz");
+            }
+            else if (number % 3 == 0)
+            {
+                Console.WriteLine("Fizz");
+            }
+            else if (number % 5 == 0)
+            {
+                Console.WriteLine("Buzz");
+            }
+            else
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
