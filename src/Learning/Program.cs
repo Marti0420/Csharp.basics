9c3971cbaf5616fe477645f848580b5a66ed655c
Martyna Piórkowska <martynapiorkowska@macbook-pro-martyna.home>
2020-04-13 19:33:19 +0200

    Add updates

--- src/Learning/Program.cs
+++ src/Learning/Program.cs
@@ -6,21 +6,27 @@ namespace Learning
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Insert sleep time");
-            var textSleepTime = Console.ReadLine();
-            var sleepTime = Convert.ToInt32(textSleepTime);
+            Console.WriteLine("When did you go to sleep?");
+            var textSleepHour = Console.ReadLine();
+            var sleepHour = Convert.ToInt16(textSleepHour);
+            
+            Console.WriteLine("When did you get up?");
+            var textWakeHour = Console.ReadLine();
+            var wakeHour = Convert.ToInt16(textWakeHour);
+
+            var sleepTime = 24 - sleepHour + wakeHour;
 
             if (sleepTime >= 12)
             {
-                Console.WriteLine($"I slept too much, I have a headache");
+                Console.WriteLine($"You slept too much. {sleepTime} hours");
             }
             else if (sleepTime >= 9)
             {
-                Console.WriteLine($"I slept good");
+                Console.WriteLine($"You slept good");
             }
             else if (sleepTime >= 4)
             {
-                Console.WriteLine($":/");
+                Console.WriteLine($"You must go to sleep, dude");
             }
             else
             {
