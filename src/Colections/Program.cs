6436b7245f196a614d5a93fadca35b43dee23e9e
Martyna Piórkowska <martynapiorkowska@macbook-pro-martyna.home>
2020-04-15 20:50:52 +0200

    Add FizzBuzz Loop

---
+++ src/Colections/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Colections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = new[] {1, 2, 3, 4};
+
+            Console.WriteLine(array[2]);
+
+            var list = new List<int>(){1,2,3,4,5,6}; 
+            Console.WriteLine(list.Count);
+
+            list.Add(7);
+            list.Add(8);
+            
+            Console.WriteLine(list.Count);
+
+            list.Remove(5);
+            
+            
+            // LOOPS
+            
+            // FOR
+
+            for (;;)
+            {
+                Console.WriteLine("INFINITE");
+            }
+
+            // WHILE
+            
+
+            // DO WHILE
+
+            // FOREACH 
+
+            foreach (var number in list)
+
+            {
+                
+            }
+        }
+    }
+}
---
+++ src/FB Collections/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace FB_Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Enumerable.Range(1, 100);
+            foreach (var number in numbers)
+            {
+                if (number % 3 == 0 && number % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (number % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (number % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(number);
+                }
+                
+            }
+        }
+    }
+}
